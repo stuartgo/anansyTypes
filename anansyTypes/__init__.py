a418aedc0856644ac2a73ea896a71c851d280514
stuartgo <ottersenstuart@gmail.com>
2025-12-07 15:44:14 +0100

    trying again

--- anansyTypes/__init__.py
+++ anansyTypes/__init__.py
@@ -1,10 +1,27 @@
-import enums
-import schemas
-import structures
+from .enums import *
+from .schemas import *
+from .structures import Embedding, Word, Symbol, Phrase
 
 
 __all__ = [
     "enums",
     "schemas",
     "structures"
+    "EmbedRequest",
+    "EmbeddingResponse",
+    "ResponseRequest",
+    "PreprocessRequest",
+    "PreprocessResponse",
+    "GetSymbolRequest",
+    "GetSymbolResponse",
+    "GetWordRequest",
+    "GetWordResponse",
+    "GetSimilarWordRequest",
+    "GetAllPhrasesResponse",
 ]
+
+
+
+
+
+
