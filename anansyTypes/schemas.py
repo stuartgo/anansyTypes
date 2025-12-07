2b4d58fb45ff601d139dea7f5a351ea0bf670fd7
stuartgo <ottersenstuart@gmail.com>
2025-12-07 15:50:11 +0100

    trying again

--- anansyTypes/schemas.py
+++ anansyTypes/schemas.py
@@ -2,46 +2,50 @@ from pydantic import BaseModel as PydanticBaseModel
 from typing import List, Any, Dict,Union
 from structures import Embedding,Word, Symbol,Phrase
 
+class ModifiedBaseModel(PydanticBaseModel):
+    class Config:
+        arbitrary_types_allowed = True
 
 
-class EmbedRequest(PydanticBaseModel):
+
+class EmbedRequest(ModifiedBaseModel):
     stemmed_words: List[str]
     tokens: dict
-class EmbeddingResponse(PydanticBaseModel):
+class EmbeddingResponse(ModifiedBaseModel):
     embeddings: List[Embedding]
 
 
-class ResponseRequest(PydanticBaseModel):
+class ResponseRequest(ModifiedBaseModel):
     text: str
     context: Dict[str, Any]
     
     
-class PreprocessRequest(PydanticBaseModel):
+class PreprocessRequest(ModifiedBaseModel):
     text: str
 
-class PreprocessResponse(PydanticBaseModel):
+class PreprocessResponse(ModifiedBaseModel):
     raw_words: List[str]
     tokens: Dict[str, Any]
     
     
-class GetSymbolRequest(PydanticBaseModel):
+class GetSymbolRequest(ModifiedBaseModel):
     lexeme_id: int
     
-class GetSymbolResponse(PydanticBaseModel):
+class GetSymbolResponse(ModifiedBaseModel):
     symbol: Symbol
     
-class GetWordRequest(PydanticBaseModel):
+class GetWordRequest(ModifiedBaseModel):
     word_text: str
     stemmed: bool = False
     
 
-class GetWordResponse(PydanticBaseModel):
+class GetWordResponse(ModifiedBaseModel):
     word:Union[Word,None]
     
-class GetSimilarWordRequest(PydanticBaseModel):
+class GetSimilarWordRequest(ModifiedBaseModel):
     embedding: Embedding
     
-class GetAllPhrasesResponse(PydanticBaseModel):
+class GetAllPhrasesResponse(ModifiedBaseModel):
     phrases: List[Phrase]
     
 
