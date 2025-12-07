b11ab25ef610cf48d1c97615a9c2f31989828a7d
stuartgo <ottersenstuart@gmail.com>
2025-12-07 15:22:24 +0100

    added content to __init__

---
+++ __init__.py
@@ -0,0 +1,10 @@
+import enums
+import schemas
+from structures import Embedding, Word, Symbol, Phrase
+
+
+__all__ = [
+    "enums",
+    "schemas",
+    "structures"
+]
---
+++ structures/__init__.py
@@ -0,0 +1,14 @@
+from embedding import Embedding
+from lexeme import Lexeme
+from word import Word
+from symbol import Symbol
+from phrase import Phrase
+
+
+__all__ = [
+    "Embedding",
+    "Lexeme",
+    "Word",
+    "Symbol",
+    "Phrase",
+]
