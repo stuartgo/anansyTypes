453d484526a029fefeb8a32cde1acb4bc87d0e6b
stuartgo <ottersenstuart@gmail.com>
2025-12-09 18:12:00 +0100

    use array instead of image

--- anansyTypes/structures/symbol.py
+++ anansyTypes/structures/symbol.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
 from typing import Optional
-from PIL import Image
-
-
+import numpy as np
+from pydantic_tensor import Tensor
+from typing import Any, Literal
+import torch
 @dataclass
 class Symbol:
     id: int
     label: str
-    image: Optional[Image.Image]
+    image: Tensor[ np.ndarray[Any, Any],tuple, Literal["int32", "int64"]]
+
