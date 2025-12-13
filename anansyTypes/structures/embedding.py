0a07ea6d39a96e6a8ac9f99b8297857de969a76d
Christoph Eder <ce1@live.de>
2025-12-13 18:09:07 +0100

    Changes in schemas.py     Trying to use proper types now Changes in embedding.py     Implemented serialisation

--- anansyTypes/structures/embedding.py
+++ anansyTypes/structures/embedding.py
@@ -26,3 +26,16 @@ class Embedding:
     @classmethod
     def from_dict(cls, data: dict) -> "Embedding":
         return cls(data=np.array(data["data"], dtype=np.float32))
+    
+    @classmethod
+    def __get_pydantic_core_schema__(cls, source_type: Any, handler):
+        from pydantic_core import core_schema
+        
+        return core_schema.with_info_plain_validator_function(
+            lambda value, _: cls.from_dict(value) if isinstance(value, dict) else value,
+            serialization=core_schema.plain_serializer_function_ser_schema(
+                lambda x: {"data": x.data.tolist()},
+                when_used="json",
+            ),
+        )
+
