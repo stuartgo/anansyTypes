6dcea55bd635cb672efc96a3541fccb421017c03
stuartgo <ottersenstuart@gmail.com>
2025-12-27 23:48:12 +0100

    add original_content to both phrase and phrasenoembedding

--- anansyTypes/structures/phrase.py
+++ anansyTypes/structures/phrase.py
@@ -39,6 +39,11 @@ class Phrase(Lexeme):
     @property
     def content(self) -> str:
         return self._content
+    
+    @property
+    def original_content(self) -> str:
+        #temporarily return content as original_content
+        return self._content
 
     @property
     def embedding(self) -> Embedding:
