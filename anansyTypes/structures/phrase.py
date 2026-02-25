7cc7d05fc679d959832ed217c00029551876883a
stuartgo <ottersenstuart@gmail.com>
2026-02-25 14:24:36 +0100

    fix phrase with new lexeme verion

--- anansyTypes/structures/phrase.py
+++ anansyTypes/structures/phrase.py
@@ -7,49 +7,12 @@ import numpy as np
 
 
 class NoEmbeddingPhrase(Lexeme):
-    def __init__(self, id: int, content: str):
-        self.id = id
-        self._content = content
-        self._original_content = content
-        self._embedding = np.array([])  # Placeholder to avoid attribute errors
-
-    @property
-    def content(self) -> str:
-        return self._content
-    
-    @property
-    def original_content(self) -> str:
-        #temporarily return content as original_content
-        return self._content
-
-    @property
-    def embedding(self) -> Embedding:
-        return self._embedding
-
     @classmethod
     def from_dict(cls, data: dict) -> "NoEmbeddingPhrase":
-        return cls(id=data["id"], content=data["content"])
+        return cls(id=data["id"], content=data["content"],embedding=None)
 
 
 class Phrase(Lexeme):
-    def __init__(self, id: int, content: str, embedding: Embedding):
-        self.id = id
-        self._content = content
-        self._original_content = content
-        self._embedding = embedding
-
-    @property
-    def content(self) -> str:
-        return self._content
-    
-    @property
-    def original_content(self) -> str:
-        #temporarily return content as original_content
-        return self._content
-
-    @property
-    def embedding(self) -> Embedding:
-        return self._embedding
 
     @classmethod
     def from_dict(cls, data: dict) -> "Phrase":
